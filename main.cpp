efaea836251037077c3eb12dba05399142c5450d
Owen Lee <gp6h06@gmail.com>
2023-07-26 17:12:52 +0800

    update countdown functions

--- main.cpp
+++ main.cpp
@@ -1,39 +1,64 @@
 #include <iostream>
 #include <ctime>
 #include <Windows.h>
-#include <string>
 
 using namespace std;
 
-string getPresentTime();
+void init();
+void setCountingTime();
+void runCountdown();
+void showTime();
+
+unsigned int seconds;
+unsigned int minutes;
+unsigned int hours;
 
 int main(){
-    int seconds;
-    cout << "Enter total number seconds for counter" << endl;
-
-   
-    if(cin){
-         cin >> seconds;
-         cout << "Counting start !! " + getPresentTime() << endl;
-    }
-     
-    while(seconds >= 1){
-        cout << "Counter : " << seconds << endl;
-        Sleep(1000);
-        seconds--;
-    }
-
-    cout << "Counting end !! "+ getPresentTime() << endl;
-   
-
+    init();
+    setCountingTime();
+    runCountdown();
     return 0;
 }
 
-string getPresentTime(){
-    time_t tt;
-    struct tm* st;
+void init(){
+    cout << "********* Welcome to CountDownTimer *********" << endl;
+    cout << "\nPlease enter the time you want to count down." << endl;
+}
 
-    time(&tt);
-    st = localtime(&tt);
-    return asctime(st);
-}   
+void setCountingTime(){
+    cout << "\nEnter Hours: " << endl;
+    cin >> hours;
+    cout << "\nEnter Minutes: " << endl;
+    cin >> minutes;
+    cout << "\nEnter Seconds: " << endl;
+    cin >> seconds;
+}
+
+void runCountdown(){
+     while(seconds >= 1){
+        showTime();
+        Sleep(1000);
+        --seconds;
+
+        if(seconds == 0 && minutes > 0){
+            --minutes;
+            seconds = 59;
+        }
+
+        if(seconds == 0 && minutes == 0 && hours > 0){
+            --hours;
+            seconds = 59;
+            minutes = 59;
+        }
+
+        if(seconds == 0 && minutes == 0 && hours == 0){
+            break;
+        }
+    }
+    cout << "********* Count Down Finished *********" << endl;
+    cout << "Stand UP and Move Move Move !!!" << endl;
+}
+
+void showTime(){
+    cout << hours << ":" << minutes << ":" << seconds << endl;
+}
